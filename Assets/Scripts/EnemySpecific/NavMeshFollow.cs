919a463bf941d49fe1b076f1c0518220c1a22a87
Haven <leoshelleyyellow8@gmail.com>
2024-02-29 05:20:21 +0000

    ENEMY CAN NOW KILL FROM ABOVE YEAAAA

--- Assets/Scripts/EnemySpecific/NavMeshFollow.cs
+++ Assets/Scripts/EnemySpecific/NavMeshFollow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class NavMeshFollow : MonoBehaviour
 {
@@ -10,17 +11,26 @@ public class NavMeshFollow : MonoBehaviour
     public Transform target;
     NavMeshAgent agent;
     Animator animator;
-    [Header("AttackVariables")]
+    EnemyHealth eH;
+    float speed;
+    [Header("Attack Variables")]
     bool isAttacking;
     public Transform attackPos;
     public Vector2 attackSize;
     public float attackRange;
     public float attackDamage;
+    public float attackWindUp;
+    public float preAttackTime;
+    public float attackDuration;
+    public float attackCooldown;
     // Start is called before the first frame update
     void Start()
     {
+        
+        eH = GetComponent<EnemyHealth>();
         animator = GetComponentInChildren<Animator>();
         agent = GetComponent<NavMeshAgent>();
+        speed = agent.speed;
     }
 
     // Update is called once per frame
@@ -33,36 +43,39 @@ public class NavMeshFollow : MonoBehaviour
             transform.rotation = transform.position.x > target.position.x ? Quaternion.Euler(0, 180, 0) : Quaternion.Euler(0, 0, 0);
             agent.SetDestination(target.position);  
         }
-        if (agent.remainingDistance <= agent.stoppingDistance && !isAttacking)
+        if (agent.remainingDistance > agent.stoppingDistance && !isAttacking && (Mathf.Abs(target.transform.position.y - transform.position.y) < 2))
         {
-            
+            agent.isStopped = true;
+            agent.speed = 0;
             isAttacking = true;
             StartCoroutine(Attack());
         }
-
+        
     }
 
     IEnumerator Attack()
     {
 
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(attackWindUp);
         animator.SetBool("Attacking", true);
-        Vector2 distance = transform.position - target.position;;
-        float t = 0;
-        while(t < 0.583f)
+        yield return new WaitForSeconds(preAttackTime);
+        RaycastHit2D[] hit = Physics2D.BoxCastAll(attackPos.position, attackSize, 0, transform.right, 0);
+        for (int i = 0; i < hit.Length; i++)
         {
-            t += Time.deltaTime;
-            RaycastHit2D hit = Physics2D.BoxCast(attackPos.position, attackSize, 0, transform.right);
-            if (hit)
+            if (hit[i].transform.tag == "Player")
             {
-                Debug.Log("hit!");
-                hit.transform.GetComponent<PlayerStats>().ChangeHealth(attackDamage, true);
+                Debug.Log(hit[i].transform.name);
+                hit[i].transform.GetComponent<PlayerStats>().ChangeHealth(attackDamage, true);
             }
         }
-        
-        yield return new WaitForSeconds(1f);
-        isAttacking=false;
+        yield return new WaitForSeconds(attackDuration);
         animator.SetBool("Attacking", false);
+        yield return new WaitForSeconds(attackCooldown);
+        
+        isAttacking = false;
+        agent.speed = speed;
+        agent.isStopped = false;
+        
     }
 
     private void OnDrawGizmos()
